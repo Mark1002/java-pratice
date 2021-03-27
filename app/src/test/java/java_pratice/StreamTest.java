02d68652ae73709dd74cb43d8a1599dbf814b83f
Mark Chang <ak0922747401@gmail.com>
2021-03-27 20:07:04 +0800

    first commit

---
+++ app/src/test/java/java_pratice/StreamTest.java
@@ -0,0 +1,11 @@
+package java_pratice;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StreamTest {
+    @Test
+    void testInt() {
+        assertEquals(10, 10);
+    }
+}
